1d7d36c90dce9c748fd4667209effcb905860b61
edwin young <edwin@bathysphere.org>
2018-12-17 13:45:20 -0800

    add analyzer project which does nothing

---
+++ src/PSRos/Analyzer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace PSRos
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PowerShellModuleAnalyzer : DiagnosticAnalyzer
+    {
+        private const string DiagnosticId = "Analyzer1";
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            id: DiagnosticId,
+            title: "Type name contains lowercase letters",
+            messageFormat: "Type name '{0}' contains lowercase letters",
+            category: Category,
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Type names should be all uppercase.");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+
+        }
+    }
+}
