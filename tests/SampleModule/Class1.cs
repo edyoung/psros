000acf3215704d7254e5fe209cbfe757ffa9b099
edwin young <edwin@bathysphere.org>
2018-12-17 12:26:30 -0800

    reference powershell assemblies

--- tests/SampleModule/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SampleModule
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ tests/SampleModule/SampleModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace SampleModule
+{
+    [Cmdlet(VerbsCommon.Get, "Foo")]
+    public class GetFooCmdlet : PSCmdlet
+    {
+
+    }
+}
